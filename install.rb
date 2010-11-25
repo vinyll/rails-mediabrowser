7ff7dc7e26373194d2a6e847b560206df759748c
Vincent Agnano <vincent.agnano@particul.es>
2010-11-25 18:04:57 +0100

    fixed install/uninstall web assets

--- install.rb
+++ install.rb
@@ -1,6 +1,11 @@
+require 'fileutils'
+
+RAILS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '/../../../')) unless defined? RAILS_ROOT
+
+
 puts 'Publishing web assets to public folder'
 
-FileUtils.ln_s(
-  File.expand(File.join(__FILE__,'..', 'public')),\
-  File.expand(File.join(RAILS_ROOT, 'public', 'mediabrowser'))
+FileUtils.cp_r(
+  File.expand_path(File.join(__FILE__, '..', 'public')),\
+  File.expand_path(File.join(RAILS_ROOT, 'public', 'mediabrowser'))
 )
--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,9 @@
-FileUtils.rm_r(File.expand(File.join(RAILS_ROOT, 'public', 'mediabrowser')))
+require 'fileutils'
+
+RAILS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '/../../../')) unless defined? RAILS_ROOT
+
+
+puts 'Removind mediabrowser web assets from public folder'
+
+
+FileUtils.rm_r(File.expand_path(File.join(RAILS_ROOT, 'public', 'mediabrowser')))
