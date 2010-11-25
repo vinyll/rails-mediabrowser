93e640d154088f578df53139f2a95fa07855297e
Vincent Agnano <vincent.agnano@particul.es>
2010-11-25 18:26:46 +0100

    fixed install chmod uploads dir issues

--- install.rb
+++ install.rb
@@ -2,10 +2,19 @@ require 'fileutils'
 
 RAILS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '/../../../')) unless defined? RAILS_ROOT
 
+public_dir = File.expand_path(File.join(RAILS_ROOT, 'public'))
 
 puts 'Publishing web assets to public folder'
-
 FileUtils.cp_r(
-  File.expand_path(File.join(__FILE__, '..', 'public')),\
-  File.expand_path(File.join(RAILS_ROOT, 'public', 'mediabrowser'))
+  File.join(File.dirname(__FILE__), 'public'),\
+  File.join(public_dir, 'mediabrowser')
 )
+
+upload_dir = File.join(public_dir, 'uploads')
+unless File.exists?(upload_dir)
+  puts 'Creating uploads directory at "%s"' % upload_dir
+  FileUtils.mkdir(upload_dir)
+end
+
+puts 'chmod 777 on uploads directory "%s"' % upload_dir
+FileUtils.chmod_R(0777, upload_dir)
--- uninstall.rb
+++ uninstall.rb
@@ -3,7 +3,9 @@ require 'fileutils'
 RAILS_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '/../../../')) unless defined? RAILS_ROOT
 
 
-puts 'Removind mediabrowser web assets from public folder'
+puts 'Removing mediabrowser web assets from public folder'
 
 
 FileUtils.rm_r(File.expand_path(File.join(RAILS_ROOT, 'public', 'mediabrowser')))
+
+puts 'the uploads folder was not removed. Delete it manually if you do not need it nomore.'
